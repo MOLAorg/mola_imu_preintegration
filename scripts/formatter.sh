bacb14861ed7743f36d4961f7cfc28facda24457
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-05-11 23:39:05 +0200

    Update formatter helper

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -15,5 +15,5 @@ find \
     include \
     src \
     tests \
-    -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \
-  -print0 | xargs -0 clang-format-14 "${MODE[@]}"
+    \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) \
+  -print0 | xargs -0 -r -t clang-format-14 "${MODE[@]}"
